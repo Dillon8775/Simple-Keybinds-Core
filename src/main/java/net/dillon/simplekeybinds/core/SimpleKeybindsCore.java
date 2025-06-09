2af3128ebc6ce610215c2ce1c368e585923a4f9c
Dillon <dilloneystrickland12@gmail.com>
2025-06-09 01:12:15 -0400

    fix!

--- src/main/java/net/dillon/simplekeybinds/core/SimpleKeybindsCore.java
+++ src/main/java/net/dillon/simplekeybinds/core/SimpleKeybindsCore.java
@@ -2,10 +2,15 @@ package net.dillon.simplekeybinds.core;
 
 import net.dillon.simplekeybinds.core.keybinds.ModKeybinds;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SimpleKeybindsCore implements ClientModInitializer {
+	public static boolean fog = true;
+	public static boolean fullBright = false;
+	public static final double minBrightness = 0.0D;
+	public static final double maxBrightness = 12.0D;
 	public static final Logger LOGGER = LogManager.getLogger("Simple Keybinds");
 
 	/**
@@ -14,5 +19,12 @@ public class SimpleKeybindsCore implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ModKeybinds.init();
+	}
+
+	/**
+	 * Returns true if the {@code speedrunner mod} is loaded.
+	 */
+	public static boolean isSpeedrunnerModLoaded() {
+		return FabricLoader.getInstance().isModLoaded("speedrunnermod");
 	}
 }
